5c867a6abfbd5f8100f0c2d49a7f4832f131aa3b
Zharov <zharov66@yandex.ru>
2024-05-09 13:40:58 +0300

    first stage

---
+++ #23 ege 2024.pas
@@ -0,0 +1,12 @@
+﻿##
+var s := ArrFill(19, 1);
+var a := 3..20;
+foreach var x in a do
+begin
+  s[x - 2] := s[x - 3];
+  if x.IsEven then
+    s[x - 2] += s[x div 2 - 2];
+  if (x = 4) or (x = 9) or (x = 16) then
+    s[x - 2] += s[ceil(sqrt(x)) - 2];
+end;
+println(s);
---
+++ пример интерфейса.pas
@@ -0,0 +1,43 @@
+﻿type 
+  IФигура = interface 
+    procedure Вывести; 
+    property Площадь: real read; 
+  end; 
+   
+  Прямоугольник = auto class(IФигура) 
+    private 
+      Длина: real; 
+      Ширина: real; 
+    public 
+      procedure Вывести := 
+          $'{Длина} x {Ширина} = {Площадь}'.Println; 
+      property Площадь: real read Длина * Ширина; 
+  end; 
+   
+  Квадрат = auto class(IФигура) 
+    private 
+      Сторона: real; 
+    public 
+      procedure Вывести := 
+          $'{Сторона} x {Сторона} = {Площадь}'.Println; 
+      property Площадь: real read Sqr(Сторона); 
+  end; 
+   
+  Круг = auto class(IФигура) 
+    private 
+      Радиус: real; 
+    public 
+      procedure Вывести := 
+         $'Pi x {Радиус}^2 = {Площадь}'.Println; 
+      property Площадь: real read Pi * Sqr(Радиус); 
+  end; 
+ 
+begin 
+  var L := new List<IФигура>; 
+  L.Add(new Прямоугольник(12, 15.5)); 
+  L.Add(new Прямоугольник(18.2, 9.4)); 
+  L.Add(new Квадрат(15)); 
+  L.Add(new Круг(23.27)); 
+  foreach var p in L do 
+    p.Вывести 
+end. 
